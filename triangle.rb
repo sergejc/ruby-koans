7c29557c0e70879bcf72b30f4abc962c6099a555
Sergej Charskij <sergej.charskij@gmail.com>
2017-04-19 20:46:25 +0100

    added about_triangle_project

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,13 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  # WRITE THIS CODE
+def triangle(*sizes)
+    case sizes.uniq.length
+    when 1 then :equilateral
+    when 2 then :isosceles
+    when 3 then :scalene
+    else "invalid"
+    end
 end
 
 # Error class used in part 2.  No need to change this code.
